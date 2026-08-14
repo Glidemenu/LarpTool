8fc1d1c66fe65cff1df22030ab5ec78fb3187dbe
bigbubbychubby <brysonwb13@gmail.com>
2026-08-13 21:12:57 -0500

    Download all files and put them in a folder.
    Copy LarpTool.exe twice and put the 2nd exe on somewhere like your desktop and you can run it from there.

---
+++ LarpTool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace LarpTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "LarpTool";
+            Banner();
+            Menu();
+            Console.ReadKey();
+
+
+        }
+
+        static void Banner()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(@"
+
+
+         
+ ██▓    ▄▄▄       ██▀███   ██▓███  ▄▄▄█████▓ ▒█████   ▒█████   ██▓    
+▓██▒   ▒████▄    ▓██ ▒ ██▒▓██░  ██▒▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    
+▒██░   ▒██  ▀█▄  ▓██ ░▄█ ▒▓██░ ██▓▒▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    
+▒██░   ░██▄▄▄▄██ ▒██▀▀█▄  ▒██▄█▓▒ ▒░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░    
+░██████▒▓█   ▓██▒░██▓ ▒██▒▒██▒ ░  ░  ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒
+░ ▒░▓  ░▒▒   ▓▒█░░ ▒▓ ░▒▓░▒▓▒░ ░  ░  ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░
+░ ░ ▒  ░ ▒   ▒▒ ░  ░▒ ░ ▒░░▒ ░         ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░
+  ░ ░    ░   ▒     ░░   ░ ░░         ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   
+    ░  ░     ░  ░   ░                           ░ ░      ░ ░      ░  ░
+                                                             larptool by bigbubbychubby
+
+");
+        }
+        static void Menu()
+        {
+            Console.WriteLine("\n1. Yo");
+            Console.WriteLine("\n2 This is just the bones of the project.");
+            Console.WriteLine("\n3. Do the rest yourself");
+        }
+    }
+}
